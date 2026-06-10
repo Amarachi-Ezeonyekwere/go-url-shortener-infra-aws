9eadf96bbe4ae97b12588201412aea8668c9a046
Amarachi Ezeonyekwere <203523295+Amarachi-Ezeonyekwere@users.noreply.github.com>
2026-06-10 09:42:12 +0100

    style: apply terraform fmt formatting

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -10,10 +10,10 @@ resource "aws_vpc" "main" {
 
 # SUBNET
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnet_cidr
-  availability_zone = var.availability_zone
-  map_public_ip_on_launch = true 
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Main"
